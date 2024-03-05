3f67e0fa2a33837cfb7cd5bb278d2d6727d03aaf
Akash Gupta <78719937+Akashthepro@users.noreply.github.com>
2024-03-04 22:14:01 -0500

    Adding JS externally

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed('#element', {
+        strings: ['Backend Web Developer', 'Graphic Designer', 'Gamer'],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true
+    });
+});
